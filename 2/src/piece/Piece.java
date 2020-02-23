6f0fc4d975edd9683315ec176b1610e1d72422ad
didgogns <didgogns@gmail.com>
2020-02-23 13:57:19 +0900

    fill piece

--- 2/src/piece/Piece.java
+++ 2/src/piece/Piece.java
@@ -13,6 +13,8 @@ public class Piece {
 
 	public Point[] points;
 	public int kind;
+	public int maxH;
+	public int maxW;
 
 	private Piece() {
 	}
@@ -20,6 +22,12 @@ public class Piece {
 	private Piece(Point[] points, int kind) {
 		this.points = points;
 		this.kind = kind;
+		this.maxH = 0;
+		this.maxW = 0;
+		for (Point point : points) {
+			maxH = Math.max(maxH, point.h);
+			maxW = Math.max(maxW, point.w);
+		}
 	}
 
 	static Point[][] pt = new Point[4][4];
@@ -70,6 +78,6 @@ public class Piece {
 		iTetromino = new Piece[] { new Piece(new Point[] { pt[0][0], pt[0][1], pt[0][2], pt[0][3] }, 7),
 				new Piece(new Point[] { pt[0][0], pt[1][0], pt[2][0], pt[3][0] }, 7) };
 
-		oTetromino = new Piece[] { new Piece(new Point[] { pt[0][0], pt[0][1], pt[1][0], pt[1][1] }, 7) };
+		oTetromino = new Piece[] { new Piece(new Point[] { pt[0][0], pt[0][1], pt[1][0], pt[1][1] }, 8) };
 	}
 }
---
+++ 2/src/piece/PieceFiller.java
@@ -0,0 +1,44 @@
+package piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import puzzle.Board;
+import puzzle.Info;
+
+public class PieceFiller {
+	public static Board fillPiece(Board oldBoard, Piece piece, int height, int width) {
+		Board board = new Board();
+		for (int h = 0; h < Info.HEIGHT; h++) {
+			for (int w = 0; w < Info.WIDTH; w++) {
+				board.board[h][w] = oldBoard.board[h][w];
+			}
+		}
+		board.count = oldBoard.count + 1;
+		if (piece == null) {
+			if (oldBoard.skipped) {
+				return null;
+			}
+			board.skipped = true;
+			return board;
+		}
+		for (Piece.Point point : piece.points) {
+			if (board.board[height + point.h][width + point.w] != 0) {
+				return null;
+			}
+			board.board[height + point.h][width + point.w] = piece.kind;
+		}
+		return board;
+	}
+	
+	public static List<Board> fillPiece(Board oldBoard, Piece piece) {
+		List<Board> boardList = new ArrayList<Board>();
+		for (int h = 0; h < Info.HEIGHT - piece.maxH; h++) {
+			for (int w = 0; w < Info.WIDTH - piece.maxW; w++) {
+				Board board = fillPiece(oldBoard, piece, h, w);
+				if (board != null) boardList.add(board);
+			}
+		}
+		return boardList;
+	}
+}
