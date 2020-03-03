63121fd8d90d68fd6d283416b4264f3e99a32d0a
didgogns <didgogns@gmail.com>
2020-03-04 07:43:10 +0900

    print time

--- 2/src/runner/Main.java
+++ 2/src/runner/Main.java
@@ -1,10 +1,15 @@
 package runner;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Runner runner = new Runner();
 		runner.runDFS();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(dtf.format(now));
 	}
 }
